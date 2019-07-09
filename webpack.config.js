97b4c37313069a96354136123d2b18b46e96a98c
Juan Camilo González <info@juancgonzalez.com>
2019-07-09 16:45:01 -0500

    add sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,60 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true }
+module.exports = (env, options) => {
+  return {
+    devtool: options.mode === 'development' ? 'eval-source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
           }
-        ]
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              autoprefixer: {
-                browsers: ['last 2 versions']
-              },
-              plugins: () => [autoprefixer]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: { minimize: true }
             }
-          },
-          {
-            loader: 'sass-loader',
-            options: {}
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-    }),
-    new MiniCssExtractPlugin()
-  ],
-  watchOptions: {
-    ignored: ['/node_modules/']
-  }
+          ]
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                autoprefixer: {
+                  browsers: ['last 2 versions']
+                },
+                plugins: () => [autoprefixer]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {}
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+      }),
+      new MiniCssExtractPlugin()
+    ],
+    watchOptions: {
+      ignored: ['/node_modules/']
+    }
+  };
 };
